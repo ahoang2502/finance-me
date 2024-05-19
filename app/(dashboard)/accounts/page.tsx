16bb5f642ca7d8407bd5cdb4f966bfa8baecfcad
ahoang2502 <athoang252@gmail.com>
2024-05-19 22:23:45 +1000

    feat: accounts page: get all accounts + loading state

--- app/(dashboard)/accounts/page.tsx
+++ app/(dashboard)/accounts/page.tsx
@@ -1,24 +1,36 @@
 "use client";
 
-import { Plus } from "lucide-react";
+import { Loader2, Plus } from "lucide-react";
 
 import { DataTable } from "@/components/DataTable";
 import { Button } from "@/components/ui/button";
 import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
 import { useNewAccount } from "@/features/accounts/hooks/useNewAccount";
-import { Payment, columns } from "./_components/Columns";
-
-const data: Payment[] = [
-  {
-    id: "728ed52f",
-    amount: 100,
-    status: "pending",
-    email: "m@example.com",
-  },
-];
+import { columns } from "./_components/Columns";
+import { useGetAccounts } from "@/features/accounts/api/useGetAccounts";
+import { Skeleton } from "@/components/ui/skeleton";
 
 const AccountsPage = () => {
   const { onOpen } = useNewAccount();
+  const accountsQuery = useGetAccounts();
+  const accounts = accountsQuery.data || [];
+
+  if (accountsQuery.isLoading)
+    return (
+      <div className="max-w-screen-2xl mx-auto w-full pb-10 -mt-24">
+        <Card className="border-none drop-shadow-sm">
+          <CardHeader className="">
+            <Skeleton className="h-8 w-48 " />
+          </CardHeader>
+
+          <CardContent>
+            <div className="h-[500px] w-full flex items-center justify-center">
+              <Loader2 className="size-6 text-slate-300 animate-spin" />
+            </div>
+          </CardContent>
+        </Card>
+      </div>
+    );
 
   return (
     <div className="max-w-screen-2xl mx-auto w-full pb-10 -mt-24">
@@ -35,8 +47,8 @@ const AccountsPage = () => {
         <CardContent>
           <DataTable
             columns={columns}
-            data={data}
-            filterKey="email"
+            data={accounts}
+            filterKey="name"
             onDelete={() => {}}
             disabled={false}
           />
