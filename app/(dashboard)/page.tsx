4383c1bd73a066ade382ba7bc55774eb8eaec3f0
ahoang2502 <athoang252@gmail.com>
2024-06-16 19:20:10 +1000

    feat: dashboard page: income, expenses charts

--- app/(dashboard)/page.tsx
+++ app/(dashboard)/page.tsx
@@ -1,11 +1,13 @@
 "use client";
 
+import { DataCharts } from "@/components/DataCharts";
 import { DataGrid } from "@/components/DataGrid";
 
 export default function DashboardPage() {
   return (
     <div className="max-w-screen-2xl mx-auto pb-10 -mt-24">
       <DataGrid />
+      <DataCharts />
     </div>
   );
 }
